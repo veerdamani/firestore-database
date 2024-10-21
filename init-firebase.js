5b15eb95b3809a60ee8718ecf81ae26d71a04967
unknown <veerdamanisutharakash435@gmail.com>
2024-10-21 16:14:13 +0500

    Firebase

--- init-firebase.js
+++ init-firebase.js
@@ -1,12 +1,12 @@
 // TODO: Replace the following with your app's Firebase project configuration
 var firebaseConfig = {
     apiKey: "AIzaSyBBjhATrKGW_DXJkrFv3OahAB8rA5tJaho",
-  authDomain: "make-form-use-of-firebase.firebaseapp.com",
-  projectId: "make-form-use-of-firebase",
-  storageBucket: "make-form-use-of-firebase.appspot.com",
-  messagingSenderId: "542963582840",
-  appId: "1:542963582840:web:3bfdff0e8dc6538ef9aa8d",
-  measurementId: "G-J6ESC5HH79"
+    authDomain: "make-form-use-of-firebase.firebaseapp.com",
+    projectId: "make-form-use-of-firebase",
+    storageBucket: "make-form-use-of-firebase.appspot.com",
+    messagingSenderId: "542963582840",
+    appId: "1:542963582840:web:3bfdff0e8dc6538ef9aa8d",
+    measurementId: "G-J6ESC5HH79"
   };
   
   // Initialize Firebase
